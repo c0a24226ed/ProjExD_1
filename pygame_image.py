9bda1f080de437eaf50c9025da7683628e11e5b6
arata <c0a24226ed@edu,teu.ac.jp>
2025-04-15 15:57:59 +0900

    練習問題7

--- pygame_image.py
+++ pygame_image.py
@@ -21,14 +21,14 @@ def main():
             if event.type == pg.QUIT:
                 return
 
-        x -= 1
 
-        screen.blit(bg_img, [x, 0])
-        screen.blit(bg_img, [x + 1600, 0])
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [-x + 1600, 0])
         screen.blit(kk_img, [300, 200])
 
         pg.display.update()
         tmr += 1
+        x += 1
         clock.tick(200)
 
 if __name__ == "__main__":
