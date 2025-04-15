cbed7c7cdfe3fbef24703ac08331ab001e7b6bae
arata <c0a24226ed@edu,teu.ac.jp>
2025-04-15 15:40:27 +0900

    練習問題６

--- pygame_image.py
+++ pygame_image.py
@@ -4,26 +4,31 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
-    clock = pg.time.Clock()
+    clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    # 練習2
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+
     tmr = 0
-    kk_img = pg.image.load("fig/3.png")  
-    kk_img = pg.transform.flip(kk_img, True, False)  
+    #練習６
+    x = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
-
+            if event.type == pg.QUIT:
+                return
+            #練習６
+            x -= 1
         screen.blit(bg_img, [0, 0])
+        # 練習4
         screen.blit(kk_img, [300, 200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1
+        # 練習5
         clock.tick(200)
-        
-
 
 if __name__ == "__main__":
     pg.init()
